39a4236b8038f447bccf485a0d40d8a46f590dad
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-12-24 23:33:46 +0900

    using LL2NUM

--- test.cpp
+++ test.cpp
@@ -1,5 +1,12 @@
 #include <ruby.h>
 
-void Init_test() {
-    
+VALUE return_ll2num() {
+    long long i = 123LL;
+    return LL2NUM(i);
+}
+
+extern "C" {
+    void Init_test() {
+        rb_define_global_function("func", RUBY_METHOD_FUNC(return_ll2num), 0);
+    }
 }
---
+++ test.rb
@@ -0,0 +1,3 @@
+require_relative './test.so'
+
+puts func()
