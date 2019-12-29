5943dac89f06321fd27c24d23c3f803af821cd4e
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-12-29 17:50:43 +0900

    example rb_obj_dup method

--- test.cpp
+++ test.cpp
@@ -6,8 +6,13 @@ VALUE return_ll2num() {
     return LL2NUM(i);
 }
 
+VALUE return_dup_object(VALUE arg) {
+    return rb_obj_dup(arg);
+}
+
 extern "C" {
     void Init_test() {
         rb_define_global_function("func", RUBY_METHOD_FUNC(return_ll2num), 0);
+        rb_define_global_function("dup_obj", RUBY_METHOD_FUNC(return_dup_object), 1);
     }
 }
--- test.rb
+++ test.rb
@@ -1,3 +1,8 @@
 require_relative './test.so'
 
-puts func()
+i = 42
+
+j = dup_obj(i)
+
+puts "Value:#{i}, ID:#{i.object_id}"
+puts "Value:#{j}, ID:#{j.object_id}"
